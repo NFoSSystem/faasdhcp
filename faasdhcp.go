76c53128d61db01f310a34ef5fe7f9aadfbd6744
Manaphy91 <alebanfi1991@hotmail.it>
2020-11-30 23:03:50 +0100

    commented faasdhcp.go

--- faasdhcp.go
+++ faasdhcp.go
@@ -13,26 +13,26 @@ import (
 	"github.com/krolaw/dhcp4"
 )
 
-func main() {
-	/*serverIp := &net.IP{192, 168, 1, 249}
-	startIp := &net.IP{192, 168, 1, 115}
-	subnetIp := &net.IP{255, 255, 255, 0}
-	routerIp := &net.IP{192, 168, 1, 254}
-	dnsIp := &net.IP{192, 168, 1, 254}
-	client := dhcpdb.NewRedisClient("localhost", 6379)
-	dhcpdb.CleanUpIpSets(client)
-	dhcpdb.CleanUpAvailableIpRange(client)
-	dhcpdb.CleanUpIpMacMapping(client)
+// func main() {
+// 	/*serverIp := &net.IP{192, 168, 1, 249}
+// 	startIp := &net.IP{192, 168, 1, 115}
+// 	subnetIp := &net.IP{255, 255, 255, 0}
+// 	routerIp := &net.IP{192, 168, 1, 254}
+// 	dnsIp := &net.IP{192, 168, 1, 254}
+// 	client := dhcpdb.NewRedisClient("localhost", 6379)
+// 	dhcpdb.CleanUpIpSets(client)
+// 	dhcpdb.CleanUpAvailableIpRange(client)
+// 	dhcpdb.CleanUpIpMacMapping(client)
 
-	dhcpdb.InitAvailableIpRange(client, 50)
+// 	dhcpdb.InitAvailableIpRange(client, 50)
 
-	handler := NewHandler(serverIp, startIp, subnetIp, routerIp, dnsIp, 50, time.Hour, client)
-	defer handler.Close()
-	log.Fatal(dhcp4.ListenAndServe(handler))*/
-	var m map[string]interface{}
+// 	handler := NewHandler(serverIp, startIp, subnetIp, routerIp, dnsIp, 50, time.Hour, client)
+// 	defer handler.Close()
+// 	log.Fatal(dhcp4.ListenAndServe(handler))*/
+// 	var m map[string]interface{}
 
-	Main(m)
-}
+// 	Main(m)
+// }
 
 func Main(obj map[string]interface{}) map[string]interface{} {
 	lIp, _ := nflib.GetLocalIpAddr()
